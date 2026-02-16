6290617cc7b7b615604d3be9292b4ccfd64c06a1
nabil-hajali <nabil2491@gmail.com>
2026-02-16 17:27:41 +0100

    start a countdown

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -6,6 +6,21 @@ const numberListEl = document.getElementById('numbers-list')
 
 
 //start a 30s countdown
+let timer = 31;
+countdownEl.innerText = --timer
+console.log(timer);
+
+const intervallId = setInterval(function(){
+    --timer
+    if(timer === -1) {
+    clearInterval(intervallId)
+    
+}else {
+    countdownEl.innerText = timer;
+}
+console.log(timer);
+
+}, 1000)
 
 /// visualize 5 random numbers
 //make 5 input appear
