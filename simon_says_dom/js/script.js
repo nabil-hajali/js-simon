d7094456bfcc7756de930cb4cc5f645a8280bfdd
nabil-hajali <nabil2491@gmail.com>
2026-02-16 16:55:11 +0100

    select the dom nodes

---
+++ simon_says_dom/js/script.js
@@ -0,0 +1,11 @@
+console.log('it works');
+//select the dom nodes
+const countdownEl = document.getElementById('countdown')
+const instructionEl = document.getElementById('instructions')
+const numberListEl = document.getElementById('numbers-list')
+
+
+//start a 30s countdown
+
+/// visualize 5 random numbers
+//make 5 input appear
